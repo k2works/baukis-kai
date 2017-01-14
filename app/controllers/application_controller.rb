7a428034c067741161cc03baffe07908056f3fc3
k2works <kakimomokuri@gmail.com>
2017-01-14 13:45:19 +0900

    スタイルシートの切り替え #1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :set_layout
+
+  private
+  def set_layout
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
+  end
 end
