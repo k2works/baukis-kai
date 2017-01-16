18f175f437e35904e89bffb5c6c5de9c619ba8e7
k2works <kakimomokuri@gmail.com>
2017-01-16 17:01:08 +0900

    フォームを作成する

--- app/controllers/staff/campaign_controller.rb
+++ app/controllers/staff/campaign_controller.rb
@@ -1,4 +1,8 @@
 class Staff::CampaignController < ApplicationController
   def index
   end
+
+  def submit
+    redirect_to :staff_root
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
     get 'campaign/index'
+    get 'campaign_submit', to: 'campaign#submit'
   end
 
   namespace :admin do
