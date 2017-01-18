768229df8ef676c58cbe95fba7f1e208a0f97e64
k2works <kakimomokuri@gmail.com>
2017-01-18 13:40:43 +0900

    ActiveSupport::Concern #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from Exception, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActionController::RoutingError, with: :rescue404
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  include ErrorHandlers if Rails.env.production?
 
   private
   def set_layout
@@ -21,18 +17,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def rescue404(e)
-    @exception = e
-    render 'errors/not_found', status: 404
-  end
-
-  def rescue403(e)
-    @exception = e
-    render 'errors/forbidden', status: 403
-  end
-
-  def rescue500(e)
-    @exception = e
-    render 'errors/internal_server_error', status: 500
-  end
 end
