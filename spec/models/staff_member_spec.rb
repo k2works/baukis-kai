aab3d61d1eff766cfa50cc2a5e76901392dc4e5e
k2works <kakimomokuri@gmail.com>
2017-01-18 18:09:49 +0900

    ハッシュ関数 #3

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -25,5 +25,20 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    # 文字列を与えると、hashed_passwordは長さ60の文字列になる
+    it 'is a string of length 60 of hashed_password when giving a string' do
+      member = StaffMember.new
+      member.password = 'baukis'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    # nilを与えると、hashed_passwordはnilになる
+    it 'is nil of hashed_password when giving a nil' do
+      member = StaffMember.new(hashed_password: 'x')
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
