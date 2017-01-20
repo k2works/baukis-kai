c4d80274c6d3606f5f4b2f4432db83cf94b06728
k2works <kakimomokuri@gmail.com>
2017-01-20 09:50:04 +0900

    コントローラベースクラス作成 #3

---
+++ app/controllers/admin/base.rb
@@ -0,0 +1,11 @@
+class Admin::Base < ApplicationController
+  private
+  def current_administrator
+    if session[:administrator_id]
+      @current_administrator ||=
+          Administrator.find_by(id: session[:administrator_id_id])
+    end
+  end
+
+  helper_method :current_administrator
+end
