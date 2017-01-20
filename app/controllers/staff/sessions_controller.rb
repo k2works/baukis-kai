839a3626de835012d1cfa40311484c01831a5155
k2works <kakimomokuri@gmail.com>
2017-01-20 20:00:50 +0900

    paramsオブジェクト #4

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -7,4 +7,26 @@ class Staff::SessionsController < Staff::Base
       render action: 'new'
     end
   end
+
+  def create
+    @form = Staff::LoginForm.new(post_params)
+    if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:staff_login_form).permit(
+                   :email,
+                   :password
+    )
+  end
+
 end
