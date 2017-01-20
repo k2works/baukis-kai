5f01188197b6d1b681bb5518f9aab553e07b82bf
k2works <kakimomokuri@gmail.com>
2017-01-20 09:36:25 +0900

    パスワードメソッド作成 #3

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -20,4 +20,11 @@ class Administrator < ApplicationRecord
     self.email_for_index = email.downcase if email
   end
 
+  def password=(raw_password)
+    if raw_password.kind_of?(String)
+      self.hashed_password = BCrypt::Password.create(raw_password)
+    elsif raw_password.nil?
+      self.hashed_password = nil
+    end
+  end
 end
