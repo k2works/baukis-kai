8774e52d36945661511db6bf578e58e9455179d1
k2works <kakimomokuri@gmail.com>
2017-01-21 17:48:31 +0900

    管理者ログイン機能追加 #4

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,40 @@
+class Admin::SessionsController < Admin::Base
+  def new
+    if current_administrator
+      redirect_to :admin_root
+    else
+      @form = Admin::LoginForm.new
+      render action: 'new'
+    end
+  end
+
+  def create
+    @form = Admin::LoginForm.new(post_params)
+    if @form.email.present?
+      administrator = Administrator.find_by(email_for_index: @form.email.downcase)
+    end
+    if administrator
+      if administrator.suspended?
+        flash.now.alert = 'アカウントが停止されています。'
+        render action: 'new'
+      else
+        session[:administrator_id] = administrator.id
+        session[:last_access_time] = Time.current
+        flash.notice = 'ログインしました。'
+        redirect_to :admin_root
+      end
+    else
+      flash.now.alert = 'メールアドレスまたはパスワードが正しくありません。'
+      render action: 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:admin_login_form).permit(
+        :email,
+        :password
+    )
+  end
+  
+end
