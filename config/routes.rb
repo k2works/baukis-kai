302bd71c58baf7f92e45fb401c9db2230fd51e96
k2works <kakimomokuri@gmail.com>
2017-01-24 17:39:00 +0900

    名前空間 #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,43 @@
 # == Route Map
 #
-#                   Prefix Verb   URI Pattern                         Controller#Action
-#     errors_routing_error GET    /errors/routing_error(.:format)     errors#routing_error
-#               staff_root GET    /staff(.:format)                    staff/top#index
-#              staff_login GET    /staff/login(.:format)              staff/sessions#new
-#            staff_session POST   /staff/session(.:format)            staff/sessions#create
-#                          DELETE /staff/session(.:format)            staff/sessions#destroy
-#               admin_root GET    /admin(.:format)                    admin/top#index
-#              admin_login GET    /admin/login(.:format)              admin/sessions#new
-#            admin_session POST   /admin/session(.:format)            admin/sessions#create
-#                          DELETE /admin/session(.:format)            admin/sessions#destroy
-#            customer_root GET    /customer(.:format)                 customer/top#index
-#   customer_article_index GET    /customer/article/index(.:format)   customer/article#index
-#  customer_campaign_index GET    /customer/campaign/index(.:format)  customer/campaign#index
-# customer_campaign_submit GET    /customer/campaign_submit(.:format) customer/campaign#submit
-#                     root GET    /                                   errors#routing_error
-#                          GET    /*anything(.:format)                errors#routing_error
+#                   Prefix Verb   URI Pattern                     Controller#Action
+#     errors_routing_error GET    /errors/routing_error(.:format) errors#routing_error
+#               staff_root GET    /staff(.:format)                staff/top#index
+#              staff_login GET    /staff/login(.:format)          staff/sessions#new
+#            staff_session POST   /staff/session(.:format)        staff/sessions#create
+#                          DELETE /staff/session(.:format)        staff/sessions#destroy
+#               admin_root GET    /admin(.:format)                admin/top#index
+#              admin_login GET    /admin/login(.:format)          admin/sessions#new
+#            admin_session POST   /admin/session(.:format)        admin/sessions#create
+#                          DELETE /admin/session(.:format)        admin/sessions#destroy
+#            customer_root GET    /                               customer/top#index
+#   customer_article_index GET    /article/index(.:format)        customer/article#index
+#  customer_campaign_index GET    /campaign/index(.:format)       customer/campaign#index
+# customer_campaign_submit GET    /campaign_submit(.:format)      customer/campaign#submit
+#                     root GET    /                               errors#routing_error
+#                          GET    /*anything(.:format)            errors#routing_error
 #
 
 Rails.application.routes.draw do
   get 'errors/routing_error'
 
-  namespace :staff do
-    root   'top#index'
+  concern :session_path do
     get    'login'   => 'sessions#new', as: :login
     post   'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
+  end
+
+  namespace :staff do
+    root   'top#index'
+    concerns :session_path
   end
 
   namespace :admin do
     root 'top#index'
-    get    'login'   => 'sessions#new', as: :login
-    post   'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    concerns :session_path
   end
 
-  namespace :customer do
+  namespace :customer, path: '' do
     root 'top#index'
     get 'article/index'
     get 'campaign/index'
