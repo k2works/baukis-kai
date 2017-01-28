0a01f987c6ff5507224625222cdf9df67acfc5dd
k2works <kakimomokuri@gmail.com>
2017-01-28 13:49:30 +0900

    updateアクション #7

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -6,4 +6,26 @@ class Staff::AccountsController < Staff::Base
   def edit
     @staff_member = current_staff_member
   end
+
+  def update
+    @staff_member = current_staff_member
+    @staff_member.assign_attributes(staff_member_params)
+    if @staff_member.save
+      flash.notice = t('.flash_success')
+      redirect_to :staff_account
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+  def staff_member_params
+    params.require(:staff_member).permit(
+                                     :email,
+                                     :family_name,
+                                     :given_name,
+                                     :family_name_kana,
+                                     :given_name_kana
+    )
+  end
 end
