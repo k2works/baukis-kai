b699d9ed54a562b269c28a83965b76b8eef3233c
k2works <kakimomokuri@gmail.com>
2017-01-28 14:58:02 +0900

    画面表示不具合修正

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
     namespace :staff, path: config[:staff][:path] do
       root   'top#index'
       concerns :session_path
-      resource :account, only: [:update]
+      resource :account, only: [:show, :edit, :update]
     end
   end
 
