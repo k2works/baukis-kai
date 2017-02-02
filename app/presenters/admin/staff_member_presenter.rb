ee6aace66dab0fded84d02783a5f45270bb43c2c
k2works <kakimomokuri@gmail.com>
2017-02-02 17:04:24 +0900

    演習問題 #11

--- app/presenters/admin/staff_member_presenter.rb
+++ app/presenters/admin/staff_member_presenter.rb
@@ -1,6 +1,14 @@
 class Admin::StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
 
+  def full_name
+    object.family_name + ' ' + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + ' ' + object.given_name_kana
+  end
+
   def suspended_mark
     suspended? ? raw('&#x2611;') : raw('&#x2610;')
   end
