2904b0189b662e5be0fe10015189c6071784c76e
k2works <kakimomokuri@gmail.com>
2017-02-03 16:23:09 +0900

    顧客アカウントの詳細表示 #13

---
+++ app/presenters/staff/address_presenter.rb
@@ -0,0 +1,16 @@
+class Staff::AddressPresenter < ModelPresenter
+  delegate :prefecture,
+           :city,
+           :address1,
+           :address2,
+           :company_name,
+           :division_name, to: :object
+
+  def postal_code
+    if md = object.postal_code.match(/\A(\d{3})(\d{4})\z/)
+      md[1] + '-' + md[2]
+    else
+      object.postal_code
+    end
+  end
+end
