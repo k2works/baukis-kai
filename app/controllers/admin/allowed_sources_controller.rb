8cf338b152d52233de9c20b00c5663e0d2522157
k2works <kakimomokuri@gmail.com>
2017-02-10 17:32:11 +0900

    許可IPアドレスの追加 #28

--- app/controllers/admin/allowed_sources_controller.rb
+++ app/controllers/admin/allowed_sources_controller.rb
@@ -3,4 +3,25 @@ class Admin::AllowedSourcesController < Admin::Base
     @allowed_sources = AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
     @new_allowed_source = AllowedSource.new
   end
+
+  def create
+    @new_allowed_source = AllowedSource.new(allowed_source_params)
+    @new_allowed_source.namespace = 'staff'
+    if @new_allowed_source.save
+      flash.notice = t('.flash_notice')
+      redirect_to action: 'index'
+    else
+      @allowed_sources = AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
+      flash.now.alert = t('.flash_alert')
+      render action: 'index'
+    end
+  end
+
+  private
+  def allowed_source_params
+    params.require(:allowed_source).permit(:octet1,
+                                           :octet2,
+                                           :octet3,
+                                           :last_octet)
+  end
 end
