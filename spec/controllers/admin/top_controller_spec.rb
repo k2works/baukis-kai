67d016b0bca9ccdb85aa6bc48d5acbca4ea2ac6f
k2works <kakimomokuri@gmail.com>
2017-02-13 17:47:30 +0900

    管理者ページに対するIPアクセス制限（テスト） #28

--- spec/controllers/admin/top_controller_spec.rb
+++ spec/controllers/admin/top_controller_spec.rb
@@ -3,6 +3,39 @@ require 'rails_helper'
 describe Admin::TopController, 'Before login' do
   let(:administrator) { create(:administrator) }
 
+  # IPアドレスによるアクセス制限
+  describe 'access restriction by IP address' do
+    before do
+      Rails.application.config.baukis_kai[:restrict_ip_addresses] = true
+    end
+
+    # 許可
+    context 'with authorization' do
+      it 'rendered top page' do
+        AllowedSource.create!(namespace: 'admin', ip_address: '0.0.0.0')
+        get :index
+        expect(response).to render_template('admin/top/index')
+      end
+    end
+
+    # 拒否
+    context 'with rejection' do
+      it 'rendered error page' do
+        AllowedSource.create!(namespace: 'admin', ip_address: '192.168.0.*')
+        get :index
+        expect(response).to render_template('errors/forbidden')
+      end
+    end
+
+    after do
+      Rails.application.config.baukis_kai[:restrict_ip_addresses] = false
+    end
+  end
+end
+
+describe Admin::TopController, 'After login' do
+  let(:administrator) { create(:administrator) }
+
   before do
     session[:administrator_id] = administrator.id
     session[:last_access_time] = 1.second.ago
