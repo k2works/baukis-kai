0e61a5cefa8c709e6fd89c58d74c9a8c1e03bc6d
k2works <kakimomokuri@gmail.com>
2017-02-14 18:24:11 +0900

    スコープの定義 #27

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,6 +1,6 @@
 class Staff::ProgramsController < Staff::Base
   def index
-    @programs = Program.order(application_start_time: :desc).includes(:registrant).page(params[:page])
+    @programs = Program.listing.page(params[:page])
   end
 
   def show
--- app/models/program.rb
+++ app/models/program.rb
@@ -23,4 +23,8 @@ class Program < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :applicants, through: :entries, source: :customer
   belongs_to :registrant, class_name: 'StaffMember'
+
+  scope :listing, -> {
+    order(application_start_time: :desc).includes(:registrant)
+  }
 end
