59015b15d50845516831a78dcc46852d600304c9
k2works <kakimomokuri@gmail.com>
2017-02-16 13:24:59 +0900

    バリデーション #26

--- app/models/program.rb
+++ app/models/program.rb
@@ -34,6 +34,30 @@ class Program < ApplicationRecord
   before_validation :set_application_start_time
   before_validation :set_application_end_time
 
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :description, presence: true, length: { maximum: 800 }
+  validates :application_start_date, :application_end_date, date_string: true
+  validates :application_start_time, date: {
+      after_or_equal_to: Time.zone.local(2000, 1, 1),
+      before: -> (obj) { 1.year.from_now },
+      allow_blank: true
+  }
+  validates :application_end_time, date: {
+      after: :application_start_time,
+      before: -> (obj) { obj.application_start_time.advance(days: 90) },
+      allow_blank: true,
+      if: -> (obj) { obj.application_start_time }
+  }
+  validates :min_number_of_participants, numericality: {
+      only_integer: true, greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 1000, allow_blank: true
+  }
+  validate do
+    if min_number_of_participants && max_number_of_participants && min_number_of_participants > max_number_of_participants
+      errors.add(:max_number_of_participants, :less_than_min_number)
+    end
+  end
+
   scope :listing, -> {
     joins('LEFT JOIN entries ON programs.id = entries.program_id')
         .select('programs.*, COUNT(entries.id) AS number_of_applicants')
--- app/presenters/staff/program_form_presenter.rb
+++ app/presenters/staff/program_form_presenter.rb
@@ -4,12 +4,18 @@ class Staff::ProgramFormPresenter < FormPresenter
       m << decorated_label(:description, I18n.t('staff.programs.form.show'), required: true)
       m << text_area(:description, hide_label: true, row: 6, style: 'width: 454px')
       m.span '(800文字以内)', class: 'instruction', style: 'float: right'
+      m << error_message_for(:description)
     end
   end
 
   def datetime_field_block(name, label_text, options = {})
     instruction = options.delete(:instruction)
-    markup(:div, class: 'AppForm__input-block') do |m|
+    if object.errors.include?("#{name}_time".to_sym)
+      html_class = 'AppForm__input-block AppForm--with-errors'
+    else
+      html_class = 'AppForm__input-block'
+    end
+    markup(:div, class: 'AppForm__input-block html_class') do |m|
       m << decorated_label("#{name}_date", label_text, options)
       if options[:class].kind_of?(String)
         classes = options[:class].strip.split + [ 'datetimepicker' ]
@@ -22,6 +28,8 @@ class Staff::ProgramFormPresenter < FormPresenter
       m << ':'
       m << form_builder.select("#{name}_minute", ('00'..'59').to_a, skip_label: true)
       m.span "(#{instruction})", class: 'instruction' if instruction
+      m << error_message_for("#{name}_time".to_sym)
+      m << error_message_for("#{name}_date".to_sym)
     end
   end
 end
