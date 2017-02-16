c951126b3c0a8e0635f0b08e5ca4772faed4ef9d
k2works <kakimomokuri@gmail.com>
2017-02-16 17:13:35 +0900

    本番環境でのIPアドレス制限を回避できるようにする

--- db/seed/production/allowed_sources.rb
+++ db/seed/production/allowed_sources.rb
@@ -1,2 +1,4 @@
 AllowedSource.create(namespace: 'admin', ip_address: '127.0.0.1')
 AllowedSource.create(namespace: 'staff', ip_address: '127.0.0.*')
+AllowedSource.create(namespace: 'admin', ip_address: '118.4.67.44')
+AllowedSource.create(namespace: 'staff', ip_address: '118.4.67.*')
