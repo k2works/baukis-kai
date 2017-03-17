7554c69713ab8351afff65a8688452514484fbad
k2works <kakimomokuri@gmail.com>
2017-03-17 15:19:49 +0900

    確認画面用プレゼンターの作成 #24

---
+++ app/presenters/customer/confirming_customer_form_presenter.rb
@@ -0,0 +1,17 @@
+class Customer::ConfirmingCustomerFormPresenter < ConfirmingUserFormPresenter
+  def birthday_field_block(name, label_text, options = {})
+    markup(:div, class: 'AppForm__input-block') do |m|
+      m << decorated_label(name, label_text, options)
+      m.div(object.send(name), class: 'field-value')
+      m << hidden_field(name)
+    end
+  end
+
+  def gender_field_block
+    markup(:div, class: 'AppForm__input-block') do |m|
+      m << decorated_label(:gender, '性別')
+      m.div(object.gender == 'male' ? '男性' : '女性', class: 'field-value')
+      m << hidden_field
+    end
+  end
+end
