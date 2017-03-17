c89c40d0829c19911bd1df5571596e371124f700
k2works <kakimomokuri@gmail.com>
2017-03-17 18:17:50 +0900

    機能追加により失敗するテストの修正 #24

--- spec/controllers/staff/accounts_controller_spec.rb
+++ spec/controllers/staff/accounts_controller_spec.rb
@@ -17,7 +17,7 @@ describe Staff::AccountsController do
     # email属性を変更する
     it 'should update email attributes' do
       params_hash.merge!(email: 'test@example.com')
-      patch :update, id: staff_member.id, staff_member: params_hash
+      patch :update, id: staff_member.id, staff_member: params_hash, commit: '更新'
       staff_member.reload
       expect(staff_member.email).to eq('test@example.com')
     end
@@ -25,14 +25,14 @@ describe Staff::AccountsController do
     # 例外ActionController::ParameterMissingが発生
     it 'should throw ActionController::ParameterMissing exception' do
       bypass_rescue
-      expect { patch :update, id: staff_member.id}.to raise_error(ActionController::ParameterMissing)
+      expect { patch :update, id: staff_member.id, commit: '更新'}.to raise_error(ActionController::ParameterMissing)
     end
 
     # end_dateの値は書き換え不可
     it 'should writable end_data value' do
       params_hash.merge!(end_date: Date.tomorrow)
       expect {
-        patch :update, id: staff_member.id, staff_member: params_hash
+        patch :update, id: staff_member.id, staff_member: params_hash, commit: '更新'
       }.not_to change { staff_member.end_date }
     end
   end
