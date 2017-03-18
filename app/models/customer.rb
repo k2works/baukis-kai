23455f67ba96ed916db36e32d8d96801e074671e
k2works <kakimomokuri@gmail.com>
2017-03-18 10:53:14 +0900

    モデル間の関連付け #23

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -46,6 +46,9 @@ class Customer < ApplicationRecord
   has_many :personal_phones, -> { where(address_id: nil).order(:id) },class_name: 'Phone', autosave: true
   has_many :entries, dependent: :destroy
   has_many :programs, through: :entries
+  has_many :messages
+  has_many :outbound_messages, class_name: 'CustomerMessage', foreign_key: 'customer_id'
+  has_many :inbound_messages, class_name: 'StaffMember', foreign_key: 'customer_id'
 
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
--- app/models/message.rb
+++ app/models/message.rb
@@ -31,4 +31,8 @@
 #
 
 class Message < ApplicationRecord
+  belongs_to :customer
+  belongs_to :staff_member
+  belongs_to :root, class_name: 'Message', foreign_key: 'root_id'
+  belongs_to :parent, class_name: 'Message', foreign_key: 'parent_id'
 end
