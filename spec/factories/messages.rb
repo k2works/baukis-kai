73d442a1574a2bdc6e2c6509948fbdba0ce34885
k2works <kakimomokuri@gmail.com>
2017-03-18 16:45:22 +0900

    パフォーマンスの計測 #22

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -31,7 +31,16 @@
 #
 
 FactoryGirl.define do
-  factory :message do
-    
+  factory :customer_message do
+    subject 'Subject'
+    body "Body.\nBody."
+    customer
+  end
+
+  factory :staff_message do
+    subject 'Subject'
+    body "Body.\nBody."
+    parent { FactoryGirl.create(:customer_message)}
+    staff_member
   end
 end
