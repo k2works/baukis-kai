4702627fde3ce64212b18eb427a3e788c6ed93af
k2works <kakimomokuri@gmail.com>
2017-03-18 15:35:50 +0900

    シードデータの投入 #22

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-table_names = %w(staff_members administrators staff_events customers allowed_sources programs entries)
+table_names = %w(staff_members administrators staff_events customers allowed_sources programs entries messages)
 table_names.each do |table_name|
   path = Rails.root.join('db','seed',Rails.env, "#{table_name}.rb")
   if File.exist?(path)
