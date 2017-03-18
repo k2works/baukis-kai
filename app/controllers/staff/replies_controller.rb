8535a3b1dac2be6760952ade76335f6f871121c2
k2works <kakimomokuri@gmail.com>
2017-03-18 18:20:32 +0900

    確認画面 #21

--- app/controllers/staff/replies_controller.rb
+++ app/controllers/staff/replies_controller.rb
@@ -5,8 +5,26 @@ class Staff::RepliesController < Staff::Base
     @reply = StaffMessage.new
   end
 
+  # POST
+  def confirm
+    @reply = StaffMessage.new(staff_message_params)
+    if @reply.valid?
+      render action: 'confirm'
+    else
+      flash.now.alert = t('.flash_notice')
+      render action: 'new'
+    end
+  end
+
   private
   def prepare_message
     @message = CustomerMessage.find(params[:message_id])
+  end
+
+  def staff_message_params
+    params.require(:staff_message).permit(
+        :subject,
+        :body
+    )
   end
 end
