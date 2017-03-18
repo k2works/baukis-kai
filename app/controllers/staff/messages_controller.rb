146393f6985a8c05b4f82b0afd751d48722fcfae
k2works <kakimomokuri@gmail.com>
2017-03-18 15:11:32 +0900

    メッセージ一覧 #22

--- app/controllers/staff/messages_controller.rb
+++ app/controllers/staff/messages_controller.rb
@@ -1,6 +1,28 @@
 class Staff::MessagesController < Staff::Base
   before_action :reject_non_xhr, only: [ :count ]
 
+  def index
+    @messages = Message.where(deleted: false).page(params[:page])
+  end
+
+  # GET
+  def inbound
+    @messages = CustomerMessage.where(deleted: false).page(params[:page])
+    render action: 'index'
+  end
+
+  # GET
+  def outbound
+    @messages = StaffMessage.where(deleted: false).page(params[:page])
+    render action: 'index'
+  end
+
+  # GET
+  def deleted
+    @messages = Message.where(deleted: true).page(params[:page])
+    render action: 'index'
+  end
+
   # GET
   def count
     render text: CustomerMessage.unprocessed.count
---
+++ app/presenters/staff/message_presenter.rb
@@ -0,0 +1,50 @@
+class Staff::MessagePresenter < ModelPresenter
+  delegate :subject, :body, to: :object
+
+  def type
+    case object
+      when CustomerMessage
+        '問い合わせ'
+      when StaffMember
+        '返信'
+      else
+        raise
+    end
+  end
+
+  def sender
+    case object
+      when CustomerMessage
+        object.customer.family_name + ' ' + object.customer.given_name
+      when StaffMember
+        object.staff_member.family_name + ' ' + object.staff_member.given_name
+      else
+        raise
+    end
+  end
+
+  def receiver
+    case object
+      when CustomerMessage
+        ''
+      when StaffMember
+        object.customer.family_name + ' ' + object.customer.given_name
+      else
+        raise
+    end
+  end
+
+  def truncated_subject
+    view_context.truncate(subject, length: 20)
+  end
+
+  def created_at
+    if object.created_at > Time.current.midnight
+      object.created_at.strftime('%H:%M:%S')
+    elsif object.created_at > 5.months.ago.beginning_of_month
+      object.created_at.strftime('%m/%d %H:%M')
+    else
+      object.created_at.strftime('%Y/%m/%d %H:%M')
+    end
+  end
+end
